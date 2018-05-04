a757a5cd46839d620701e848445579b782e95f7d
Ruben Blanco <ruben.blanco@xeridia.com>
2018-05-04 11:03:22 +0200

    Primer commit

---
+++ Release/sloeber.ino.cpp.d
@@ -0,0 +1,40 @@
+sloeber.ino.cpp.o: ..\sloeber.ino.cpp \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/binary.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/WCharacter.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/WString.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/HardwareSerial.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Stream.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Print.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Printable.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/USBAPI.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\variants\eightanaloginputs/pins_arduino.h \
+ P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\variants\eightanaloginputs/../standard/pins_arduino.h \
+ ..\Blink.ino
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/binary.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/WCharacter.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/WString.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/HardwareSerial.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Stream.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Print.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Printable.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/USBAPI.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\cores\arduino/Arduino.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\variants\eightanaloginputs/pins_arduino.h:
+
+P:\sloeber\arduinoPlugin\packages\arduino\hardware\avr\1.6.21\variants\eightanaloginputs/../standard/pins_arduino.h:
+
+..\Blink.ino:
